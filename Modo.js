4e35511fa8d2c16b63ae51e7eb82789505a075a3
Steven Orozco <102919020+Stiiven3@users.noreply.github.com>
2023-11-22 00:26:09 -0400

    Add files via upload

---
+++ Modo.js
@@ -0,0 +1,13 @@
+const btnSwitch=document.querySelector('#switch');
+
+btnSwitch.addEventListener('click',() => {
+    document.body.classList.toggle('dark');
+    btnSwitch.classList.toggle('active');
+  
+ 
+});
+
+
+
+
+   
